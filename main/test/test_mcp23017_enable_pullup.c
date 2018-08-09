71fbf36df88c2f963b0842470ec977331d2e0ff1
Tomoyuki Sakurai <y@trombik.org>
2018-08-09 20:02:18 +0900

    bugfix: always use int32_t as return value

--- main/test/test_mcp23017_enable_pullup.c
+++ main/test/test_mcp23017_enable_pullup.c
@@ -6,10 +6,9 @@
 
 #include "TRB_MCP23017.h"
 
-static char component[] = "[TRB_MCP23017]";
-uint32_t err;
+static char component[] = "[TRB_MCP23017][mcp23017_enable_pullup]";
+int32_t err, faked_return_value;
 uint8_t faked_reg_value;
-uint32_t faked_return_value;
 
 /* setup fff */
 DEFINE_FFF_GLOBALS;
@@ -30,6 +29,36 @@ TEST_CASE("when_pullup_a_pin_THEN_write_1_to_GPPU", component)
 	TEST_ASSERT_EQUAL_INT32(0, err);
 	TEST_ASSERT_EQUAL_UINT8(MCP23x17_GPPUA, mcp23017_write8_fake.arg0_history[0]);
 	TEST_ASSERT_EQUAL_UINT8(0b00000010, mcp23017_write8_fake.arg1_history[0]);
+
+	my_teardown();
+}
+
+TEST_CASE("when_mcp23017_read8_failed_THEN_returns_non_zero", component)
+{
+	my_setup();
+
+	/* when mcp23017_read8() fails */
+	faked_reg_value = 0xff;
+	faked_return_value = -1;
+	mcp23017_read8_fake.custom_fake = mcp23017_read8_fake_custom_fake;
+	err = mcp23017_enable_pullup(1);
+
+	/* then returns non-zero */
+	TEST_ASSERT_EQUAL_INT32(-1, err);
+
+	my_teardown();
+}
+
+TEST_CASE("when_mcp23017_write8_failed_THEN_returns_non_zero", component)
+{
+	my_setup();
+
+	/* when mcp23017_write8() fails */
+	mcp23017_write8_fake.return_val = -1;
+	err = mcp23017_enable_pullup(1);
+
+	/* then returns non-zero */
+	TEST_ASSERT_EQUAL_INT32(-1, err);
 
 	my_teardown();
 }
