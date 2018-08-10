074346383c5ee63f8cef57981ab9e42e9d76cbb8
Tomoyuki Sakurai <y@trombik.org>
2018-08-10 19:48:24 +0900

    bugfix: add src filter
    because `components` directory which has a recursive symlink. PIO
    library manager recursively dig into the directory, and fails with "Too
    many levels of symbolic links".

---
+++ prebuild.py
@@ -0,0 +1,9 @@
+Import("env")
+src_filter = ["+<TRB_MCP23017.h>"]
+env.Replace(SRC_FILTER=src_filter)
+
+build_flags = env.ParseFlags(env['BUILD_FLAGS'])
+cppdefines = build_flags.get("CPPDEFINES")
+
+if "TRB_MCP23017_ESP_IDF" in cppdefines:
+    env.Append(SRC_FILTER=["+<TRB_MCP23017.c>", "+<sys/esp_idf>"])
