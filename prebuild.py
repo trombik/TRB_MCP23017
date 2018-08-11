973b9bde6f13f49b7ae9d590da163dbdf2c1856a
Tomoyuki Sakurai <y@trombik.org>
2018-08-11 18:23:37 +0900

    remove framework-specific files from main library
    so that unit test can fake read8() and write8()

--- prebuild.py
+++ prebuild.py
@@ -6,6 +6,6 @@ build_flags = env.ParseFlags(env['BUILD_FLAGS'])
 cppdefines = build_flags.get("CPPDEFINES")
 
 if "TRB_MCP23017_ESP_IDF" in cppdefines:
-    env.Append(SRC_FILTER=["+<TRB_MCP23017.c>"])
+    env.Append(SRC_FILTER=["+<TRB_MCP23017.c>", "+<sys/esp_idf>"])
 if "TRB_MCP23017_ARDUINO_WIRE" in cppdefines:
-    env.Append(SRC_FILTER=["+<TRB_MCP23017.cpp>"])
+    env.Append(SRC_FILTER=["+<TRB_MCP23017.cpp>", "+<sys/arduino_wire>"])
